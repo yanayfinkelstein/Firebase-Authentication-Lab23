4398bbc0e2384bbbc0210cd9db77bc1841ec2c51
Yanayfi24-meet <109971369+yanayfinkelstein@users.noreply.github.com>
2023-07-24 12:15:07 +0300

    Update app.py

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -2,16 +2,17 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask import session as login_session
 import pyrebase
 config = {
-    apiKey: "AIzaSyBl_uoeLcBeD5AkpLdgAw-PuXNp49HS6qk",
-    authDomain: "fir-lab-d3e7b.firebaseapp.com",
-    projectId: "fir-lab-d3e7b",
-    storageBucket: "fir-lab-d3e7b.appspot.com",
-    messagingSenderId: "403423018058",
-    appId: "1:403423018058:web:50cf569b17ac4411797ab3",
-    measurementId: "G-QE1P8B068F"
+    "apiKey": "AIzaSyBl_uoeLcBeD5AkpLdgAw-PuXNp49HS6qk",
+    "authDomain": "fir-lab-d3e7b.firebaseapp.com",
+    "projectId": "fir-lab-d3e7b",
+    "storageBucket": "fir-lab-d3e7b.appspot.com",
+    "messagingSenderId": "403423018058",
+    "appId": "1:403423018058:web:50cf569b17ac4411797ab3",
+    "measurementId": "G-QE1P8B068F",
+    "databaseURL":""
 }
 firebase = pyrebase.initialize_app(config)
-auth = pyrebase.auth()
+auth = firebase.auth()
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
@@ -23,7 +24,16 @@ def signin():
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    
+    if method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        try:
+            login_session['user']=auth.create_user_with_email_and_password(email, password)
+            return redirect(url_for('signin'))
+        except: 
+            return render_template("signup.html")
+
+    return render_template("signup.html")
 
 
 @app.route('/add_tweet', methods=['GET', 'POST'])
